4ce3c67f89031f70e1c8f31d622968daa1d03c0e
erez <erez2613@gmail.com>
2015-03-02 03:59:06 +0200

    some ltl chnges

--- functions.php
+++ functions.php
@@ -130,7 +130,7 @@ function sh_theme_scripts() {
     wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '',true );
     
     /* - - - - plugins js - - - - - */
-    wp_enqueue_script( 'live', get_template_directory_uri() . '/js/live.js', array(), '',true ); 
+    //wp_enqueue_script( 'live', get_template_directory_uri() . '/js/live.js', array(), '',true ); 
     wp_enqueue_script( 'slick', get_template_directory_uri() . '/js/slick.min.js', array('jquery'), '',true );
     wp_enqueue_script( 'backstretch', get_template_directory_uri() . '/js/jquery.backstretch.min.js', array('jquery'), '',true );
     wp_enqueue_script( 'chosen_jquery', get_template_directory_uri() . '/js/chosen.jquery.min.js', array('jquery'), '',true );
--- js/theme.js
+++ js/theme.js
@@ -2,13 +2,7 @@
  
   $(document).ready(function(){
    
-    //scroling in menu
-     $('.menu a').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top-120
-        }, 750,'swing');
-        return false;
-     });  
+     
       
       
   //end of document.ready function
