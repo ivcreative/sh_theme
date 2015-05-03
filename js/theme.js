4b2dce601fd27bb8d01f5fedd9000dd000ca8c09
erez <erez2613@gmail.com>
2015-05-03 19:29:42 +0300

    add scrool to div

--- js/theme.js
+++ js/theme.js
@@ -5,7 +5,11 @@
     //Bootstrap Tab Collapse
     $('.nav.nav-tabs').tabCollapse();
      
-      
+    $(".downArrCont img").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".downArrCont").offset().top -20
+        }, 750);
+    });  
       
   //end of document.ready function
   });
