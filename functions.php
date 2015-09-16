428e96fbc299dfcc8565490e8333ce4ac14d42eb
erez <erez2613@gmail.com>
2015-09-16 17:14:15 +0300

    enable bootstrap again

--- functions.php
+++ functions.php
@@ -107,10 +107,10 @@ add_action( 'widgets_init', 'sh_theme_widgets_init' );
 function sh_theme_scripts() {
     
     /* - - - - styles - - - - - */
-    /*wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
     if(is_rtl()){
         wp_enqueue_style( 'bootstrap-rtl-style', get_template_directory_uri() . '/css/bootstrap-rtl.min.css' );
-    }*/
+    }
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
    
     
@@ -130,7 +130,7 @@ function sh_theme_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-    /*wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '',true );*/
+    wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '',true );
     
     /* - - - - plugins js - - - - - */
     if($_SERVER['REMOTE_ADDR']=='127.0.0.1'){
