1a2a1417de3cf163f8e415b9f5100d2f6ab57266
erez <erez2613@gmail.com>
2015-10-18 23:41:02 +0300

    some useful wp tricks in functions

--- functions.php
+++ functions.php
@@ -192,3 +192,59 @@ function cc_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/*
+Multiple excerpt lengths in wordpress
+from http://stackoverflow.com/a/4086542/2721050
+
+then in your template code you just use...
+<?php echo excerpt(25); ?>
+
+*/
+function excerpt($limit) {
+      $excerpt = explode(' ', get_the_excerpt(), $limit);
+      if (count($excerpt)>=$limit) {
+        array_pop($excerpt);
+        $excerpt = implode(" ",$excerpt).'...';
+      } else {
+        $excerpt = implode(" ",$excerpt);
+      } 
+      $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+      return $excerpt;
+    }
+
+    function content($limit) {
+      $content = explode(' ', get_the_content(), $limit);
+      if (count($content)>=$limit) {
+        array_pop($content);
+        $content = implode(" ",$content).'...';
+      } else {
+        $content = implode(" ",$content);
+      } 
+      $content = preg_replace('/\[.+\]/','', $content);
+      $content = apply_filters('the_content', $content); 
+      $content = str_replace(']]>', ']]&gt;', $content);
+      return $content;
+    }
+
+/*
+WordPress is_child() function
+http://www.kevinleary.net/wordpress-is_child-for-advanced-navigation/
+*/
+function is_child( $parent = '' ) {
+    global $post;
+
+    $parent_obj = get_page( $post->post_parent, ARRAY_A );
+    $parent = (string) $parent;
+    $parent_array = (array) $parent;
+
+    if ( in_array( (string) $parent_obj['ID'], $parent_array ) ) {
+        return true;
+    } elseif ( in_array( (string) $parent_obj['post_title'], $parent_array ) ) {
+        return true;    
+    } elseif ( in_array( (string) $parent_obj['post_name'], $parent_array ) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
