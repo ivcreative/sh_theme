dd8219a686a45af8e8e5f172198850155c50b403
erez <erez2613@gmail.com>
2015-10-26 15:26:22 +0200

    bootstrap sub menu effect and share links module

---
+++ inc/share.php
@@ -0,0 +1,47 @@
+<div class="share_cont">
+   <!--
+   you can use this styles for social
+   i{
+            color: #fff;
+            background-color: #000;
+            padding: 10px;
+            @include border-radius(50%);
+            font-size: 14px;
+            width: 35px;
+            height: 35px;
+            text-align: center;
+            &.fa-facebook{
+                background-color: #3460a1;
+            }
+            &.fa-google-plus{
+                background-color: #de5543;
+            }
+            &.fa-linkedin{
+                background-color: #136d9d;
+            }
+            &.fa-twitter{
+                background-color: #28aae1;
+            }
+             &.fa-envelope-o{
+                background-color: #bcbbbb;
+            }
+            
+        }
+   -->
+    <p><?php echo __('Share this with friends!', 'sh-theme'); ?></p>
+    <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink();?>">
+        <i class="fa fa-facebook"></i> <span>Facebook</span>
+    </a>
+    <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink();?>">
+        <i class="fa fa-google-plus"></i> <span>Google+</span>
+    </a>
+    <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink();?>&title=<?php the_title();?>">
+        <i class="fa fa-linkedin"></i> <span>Linkdin</span>
+    </a>
+    <a target="_blank" href="https://twitter.com/home?status=<?php echo get_permalink();?>">
+        <i class="fa fa-twitter"></i> <span>Twitter</span>
+    </a>
+    <a target="_blank" href="mailto:?&subject=<?php the_title();?>&body=Check%20out%20this%20URL: <?php echo get_permalink();?>">
+        <i class="fa fa-envelope-o"></i> <span>Email</span>
+    </a>
+</div>
